28a8e0fb8aadac7897849c025f02b858cc8f9266
Nathaniel Wood <nathanielwood@gmail.com>
2016-03-03 12:07:35 -0500

    fixed seed

--- seed/seed.js
+++ seed/seed.js
@@ -1,73 +1,25 @@
 /* eslint no-console:0, dot-notation:0 */
 
 import mongoose from 'mongoose';
-import Question from '../src/server/models/Question';
+import Question from '../data/models/Question';
 
 mongoose.connect('mongodb://localhost/trivia');
 
 const questions = [
   {
     text: 'A banana is a type of what?',
-    answers: [
-      {
-        value: 'Fruit',
-        correct: true,
-      },
-      {
-        value: 'Vegetable',
-        correct: false,
-      },
-      {
-        value: 'Meat',
-        correct: false,
-      },
-      {
-        value: 'Dairy',
-        correct: false,
-      },
-    ],
+    correctAnswers: ['Fruit'],
+    incorrectAnswers: ['Vegetable', 'Meat', 'Dairy'],
   },
   {
     text: 'A fish lives where?',
-    answers: [
-      {
-        value: 'In water',
-        correct: true,
-      },
-      {
-        value: 'On land',
-        correct: false,
-      },
-      {
-        value: 'In the air',
-        correct: false,
-      },
-      {
-        value: 'In space',
-        correct: false,
-      },
-    ],
+    correctAnswers: ['In water'],
+    incorrectAnswers: ['On land', 'In the air', 'In space'],
   },
   {
     text: 'What is not part of the body?',
-    answers: [
-      {
-        value: 'Mitten',
-        correct: true,
-      },
-      {
-        value: 'Head',
-        correct: false,
-      },
-      {
-        value: 'Foot',
-        correct: false,
-      },
-      {
-        value: 'Arm',
-        correct: false,
-      },
-    ],
+    correctAnswers: ['Mitten'],
+    incorrectAnswers: ['Head', 'Foot', 'Arm'],
   },
 ];
 
